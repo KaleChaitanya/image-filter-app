0159a9ba4c8d2b49eb6f4414b83f2ef4fc372a0b
Chaitanya Kale <kale.chaitanya0304@gmail.com>
2023-11-04 12:04:15 +0530

    firsrt commit

---
+++ src/componet/Shop.js
@@ -0,0 +1,75 @@
+const Shop = [
+    {
+        id: 1,
+        image: 'images/black-pant.JPG',
+        name: 'Black Pant',
+        category: 'pants',
+        price: "450₹",
+        discription: "This is black pant for men"
+    },
+    {
+        id: 2,
+        image: 'images/black-t-shirt.JPG',
+        name: 'Black T-Shirt',
+        category: 'T-Shirt',
+        price: "200₹",
+        discription: "This is black T-shirt for men"
+    },
+    {
+        id: 3,
+        image: 'images/blue-pant.JPG',
+        name: 'Blue Pant',
+        category: 'pants',
+        price: "500₹",
+        discription: "This is blue pant for men"
+    },
+    {
+        id: 4,
+        image: 'images/grey-pant.JPG',
+        name: 'grey Pant',
+        category: 'pants',
+        price: "350₹",
+        discription: "This is grey pant for men"
+    },
+    {
+        id: 5,
+        image: 'images/red-pant.JPG',
+        name: 'red Pant',
+        category: 'pants',
+        price: "450₹",
+        discription: "This is red pant for men"
+    },
+    {
+        id: 6,
+        image: 'images/red-T-shirt.png',
+        name: 'Red T-Shirt',
+        category: 'T-Shirt',
+        price: "250₹",
+        discription: "This is red T-shirt for men"
+    },
+    {
+        id: 7,
+        image: 'images/white-t-shirt.JPG',
+        name: 'White T-Shirt',
+        category: 'T-Shirt',
+        price: "300₹",
+        discription: "This is white T-shirt for men"
+    },
+    {
+        id: 8,
+        image: 'images/yellow-t-shirt.JPG',
+        name: 'Yellow T-Shirt',
+        category: 'T-Shirt',
+        price: "350₹",
+        discription: "This is yellow T-shirt for men"
+    },
+    {
+        id: 9,
+        image: 'images/yellow-t-shirt.JPG',
+        name: 'Yellow T-Shirt',
+        category: 'Shoe',
+        price: "350₹",
+        discription: "This is yellow T-shirt for men"
+    },
+]
+export default Shop
